b8536a5e30ba25eb1a22c8c41e3f7f0ef92d083d
Komandir2002 <temirlanlive2002@gmail.com>
2022-07-29 12:09:22 +0600

    fix auth

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,3 @@
-from walkeat import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -8,17 +7,11 @@ from django.contrib.auth.models import (
 import jwt
 from phonenumber_field.modelfields import PhoneNumberField
 from datetime import datetime, timedelta
-
-
-
+from walkeat import settings
 class MyUserManager(BaseUserManager):
-
-
-    def _create_user(self, email, phone, username, password, **extra_fields):
-
+    def _create_user(self, email, username, password, phone, **extra_fields):
         if not email:
             raise ValueError("Вы не ввели Email")
-
         if not username:
             raise ValueError("Вы не ввели Логин")
         user = self.model(
@@ -31,58 +24,43 @@ class MyUserManager(BaseUserManager):
             self.model(
                 email=self.normalize_email(email),
                 username=username,
-                phone=phone,
+                phone=username,
                 **extra_fields,
             )
-
             user.set_password(password)
             user.save(using=self._db)
             return user
         user.set_password(password)
         user.save(using=self._db)
-
         return user
-
-
-    def create_user(self, email, phone, username, password):
-        return self._create_user(email, phone, username, password)
-
-    def create_superuser(self, email, username, password, phone = None):
-            return self._create_user(email, phone, username, password,is_staff=True, is_superuser=True)
-
-
-
+    def create_user(self, email, username, phone, password):
+        return self._create_user(email, username, phone, password)
+    def create_superuser(self, email, username, password, phone=None):
+        if User.is_superuser:
+            phone = username
+        return self._create_user(
+            email, username, password, phone, is_staff=True, is_superuser=True
+        )
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True, unique=True)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
-    phone = PhoneNumberField(unique=True, null=True)
+    phone = PhoneNumberField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     USERNAME_FIELD = "phone"
-    REQUIRED_FIELDS = ["email", "username"]
-    if is_staff:
-        USERNAME_FIELD = "username"
-        REQUIRED_FIELDS = ["email", "phone"]
-
-
+    REQUIRED_FIELDS = ["username"]
     objects = MyUserManager()
-
     def __str__(self):
         return self.email
-
     @property
     def token(self):
         return self._generate_jwt_token()
-
     def _generate_jwt_token(self):
         dt = datetime.now() + timedelta(days=1)
-
         token = jwt.encode(
             {"id": self.pk, "exp": int(dt.strftime("%s"))},
             settings.SECRET_KEY,
             algorithm="HS256",
         )
-
         return token
--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,25 +1,16 @@
 from rest_framework import serializers
 from .models import User
-
 from django.contrib.auth import authenticate
-
-
 class UserRegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField()
-
     class Meta:
         model = User
-
-        fields = ["email", "username", "phone","password", "password2"]
-
-
-
+        fields = ["email", "username", "phone", "password", "password2"]
     def save(self, *args, **kwargs):
         user = User(
-            email=self.validated_data["email"],
-            username=self.validated_data["username"],
-
-            phone=self.validated_data["phone"]
+            email=self.validated_data["email"],  # Назначаем Email
+            username=self.validated_data["username"],  # Назначаем Логин
+            phone=self.validated_data["phone"],
         )
         password = self.validated_data["password"]
         password2 = self.validated_data["password2"]
@@ -28,30 +19,24 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-
-
 class LoginSerializer(serializers.Serializer):
     phone = serializers.CharField()
+    username = serializers.CharField(max_length=255, read_only=True, required=False)
     password = serializers.CharField(max_length=128, write_only=True)
     token = serializers.CharField(max_length=255, read_only=True)
-
     def validate(self, data):
-
         phone = data.get("phone", None)
         password = data.get("password", None)
-        user = authenticate(phone=phone, password=password)
-
+        if phone is None:
+            raise serializers.ValidationError("A phone number is required to log in.")
         if password is None:
             raise serializers.ValidationError("A password is required to log in.")
-
+        user = authenticate(phone=phone, password=password)
         if user is None:
             raise serializers.ValidationError("A user was not found.")
-
         if not user.is_active:
             raise serializers.ValidationError("This user has been deactivated.")
         return {
-            "token": user.token,
             "phone": user.phone,
-
+            "token": user.token,
         }
-

af319d92c7218f7db505d901528c07875c51089f
Komandir2002 <temirlanlive2002@gmail.com>
2022-08-02 11:16:12 +0600

    changed url

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,6 @@ from .views import RegisterUserView, LoginAPIView
 from rest_framework.routers import SimpleRouter
 
 urlpatterns = [
-    path("ap1/v1/register/", RegisterUserView.as_view(), name="register"),
-    path("ap1/v1/login/", LoginAPIView.as_view(), name="login"),
+    path("api/v1/register/", RegisterUserView.as_view(), name="register"),
+    path("api/v1/login/", LoginAPIView.as_view(), name="login"),
 ]
